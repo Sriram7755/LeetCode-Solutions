8307510f82519cf35abd132e6c0f37868e08dd5e
SRIRAM S <sriramsettu7755@gmail.com>
2026-07-12 19:11:02 +0530

    Add Two Sum

---
+++ DSA/Arrays/Two Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0;i<nums.length;i++)
+        {
+            int remain = target-nums[i];
+
+            if(map.containsKey(remain))
+            {
+                return new int[] {i,map.get(remain)};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+
+        return new int[] { -1,-1};
+    }
+}
